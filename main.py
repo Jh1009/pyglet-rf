d9b534be7c1b28fbab8fdfbb19beae0c0b2bf776
Jiatong He <978719105@qq.com>
2023-05-17 16:22:13 +0800

    实际场景调整，雷达设置，文本框调用

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import math
 import random
 import pyglet
+from pyglet.image import ImageGrid
 from pyglet.window import Window, mouse, gl
 from pyglet.image.codecs.png import PNGImageDecoder
+from pyglet.shapes import Circle
+from pyglet.text import Label
 
 win_width = 1280
 win_height = 720
@@ -28,8 +31,11 @@ red3_speed = [random.randint(-5, -1), 0]
 # 加载蓝色飞机
 blue = pyglet.image.load('fig/260_0000_蓝色飞机.png', decoder=PNGImageDecoder())
 # blue1从厦门港口出发
-blue1 = pyglet.sprite.Sprite(blue, x=307, y=370)
+blue1 = pyglet.sprite.Sprite(blue, x=1200, y=600)
 blue1.scale = 0.1
+# Create a black circle around blue1
+blue1_radar_radius = 100
+blue1_radar = Circle(blue1.x, blue1.y, blue1_radar_radius, color=(0, 0, 100, 100))
 
 blue2 = pyglet.sprite.Sprite(blue, x=260, y=440)
 blue2.scale = 0.1
@@ -50,14 +56,24 @@ red3.scale = 0.1
 
 # 加载舰艇
 boat = pyglet.image.load('fig/舰艇.png', decoder=PNGImageDecoder())
-boat1 = pyglet.sprite.Sprite(boat, x=1200, y=600)
+boat1 = pyglet.sprite.Sprite(boat, x=307, y=370)
 boat1.scale = 0.1
+# Create a black circle around boat1
+boat1_radar_radius = 150
+boat1_radar = Circle(boat1.x, boat1.y, boat1_radar_radius, color=(100, 0, 0, 100))
 
 # 飞机,舰船移动速度
-p_speed = 5
+p_speed = 4.5
 b_speed = 3
 
 x, y = 0, 0
+
+text = Label('Mission Complete!',
+             font_name='Arial',
+             font_size=36,
+             color=(0, 0, 0, 255),
+             x=window.width // 2, y=window.height // 2,
+             anchor_x='center', anchor_y='center')
 
 
 # 给窗口绑定鼠标事件
@@ -69,6 +85,12 @@ def on_mouse_motion(_x, _y, dx, dy):
 
 
 @window.event
+def on_mouse_press(x, y, button, modifiers):
+    # Handle mouse press events here
+    pass
+
+
+@window.event
 def on_draw():
     window.clear()
     pyglet.gl.glClearColor(255, 255, 255, 255)
@@ -76,6 +98,8 @@ def on_draw():
     bg.draw()
     # 绘制blue1飞机
     blue1.draw()
+    # Draw the black circle around blue1
+    blue1_radar.draw()
     # blue2.draw()
     # blue3.draw()
     # 绘制蓝色飞机
@@ -84,6 +108,9 @@ def on_draw():
     # red3.draw()
     # 加载舰艇
     boat1.draw()
+    boat1_radar.draw()
+    # Draw the text box
+    text.draw()
 
 
 # 边界检测
@@ -109,22 +136,25 @@ def move(plane, speed):
 
 
 def update(dt):
-    global x, y, p_speed, b_speed, blue2_speed, blue3_speed, red1_speed, red2_speed, red3_speed, boat1_speed
+    global x, y, p_speed, b_speed, blue2_speed, blue3_speed, red1_speed, red2_speed, red3_speed
 
-    # =========blue1移动===============
-    # 计算blue1需要移动的距离和方向
-    dx = x - blue1.x
-    dy = y - blue1.y
+    # =========舰艇移动===============
+    # 计算舰艇需要移动的距离和方向
+    # Center the boat1 sprite on its own coordinates
+    dx = x - boat1.x
+    dy = y - boat1.y
     distance = math.sqrt(dx ** 2 + dy ** 2)
     direction_x = dx / distance
     direction_y = dy / distance
     # 将direction_x,direction_y转化成角度，正上方为0度，顺时针增加
     angle = math.degrees(math.atan2(direction_y, direction_x))
-    # 调整blue1朝向角度
-    blue1.rotation = 90 - angle
-    # 将blue1的速度设置为移动方向的向量
-    blue1_speed = [direction_x * p_speed, direction_y * p_speed]
-    move(blue1, blue1_speed)
+    # 调整舰艇朝向角度
+    boat1.rotation = - angle
+    print(boat1.rotation)
+    # 将舰艇的速度设置为移动方向的向量
+    boat1_speed = [direction_x * b_speed, direction_y * b_speed]
+    move(boat1, boat1_speed)
+    move(boat1_radar, boat1_speed)
 
     # 飞机的移动
     blue2.rotation = 90
@@ -156,24 +186,26 @@ def update(dt):
     # 飞机的边界检测
     red3_speed = check_border(red3, red3_speed)
 
-    # =========舰艇的移动===============
+    # =========飞机的移动===============
     # 舰艇以固定速度向台湾岛台北市(685，435)移动
-    dx = 685 - boat1.x
-    dy = 435 - boat1.y
+    dx = 685 - blue1.x
+    dy = 435 - blue1.y
     distance = math.sqrt(dx ** 2 + dy ** 2)
     direction_x = dx / distance
     direction_y = dy / distance
     # 将direction_x,direction_y转化成角度，正上方为0度，顺时针增加
     angle = math.degrees(math.atan2(direction_y, direction_x))
     # print(angle)
-    # 调整舰艇的朝向角度
-    boat1.rotation = 135 + angle
-    # 将舰艇的速度设置为移动方向的向量
-    boat1_speed = [direction_x * b_speed, direction_y * b_speed]
-    # 舰艇的移动
-    move(boat1, boat1_speed)
-    # 舰艇的边界检测
-    boat1_speed = check_border(boat1, boat1_speed)
+    # 调整飞机的朝向角度
+    blue1.rotation = 90 - angle
+    # 将飞机的速度设置为移动方向的向量
+    blue1_speed = [direction_x * p_speed, direction_y * p_speed]
+    # 飞机的移动
+    move(blue1, blue1_speed)
+    # 飞机雷达的移动
+    move(blue1_radar, blue1_speed)
+    # 飞机的边界检测
+    # boat1_speed = check_border(boat1, boat1_speed)
 
     on_draw()
 
